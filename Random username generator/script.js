cd0ea914bc0fae9c03ffb8d59834cf5872cc861d
Devansh Biyani <devanshbiyani@gmail.com>
2023-01-18 17:23:18 +0530

    Day 3

--- Random username generator/script.js
+++ Random username generator/script.js
@@ -1,8 +1,7 @@
 const suffixes = ["_123", "_cool", "_funny", "_nerd", "_pro"];
-
-document.getElementById("generate-button").addEventListener("click", function() {
-  const prefix = document.getElementById("username-prefix").value;
-  const suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
-  const generatedUsername = prefix + suffix;
-  document.getElementById("generated-username").innerHTML = generatedUsername;
-});
+function generateUsername() {
+  var prefix = document.getElementById("prefixInput").value;
+  var randomNumber = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+  var username = prefix + randomNumber;
+  document.getElementById("generatedUsername").innerHTML = "Your username: " + username;
+}
